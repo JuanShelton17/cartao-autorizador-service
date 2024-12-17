1f0ea35d32c606f6e717087248638afa23ceb241
juan <juan.shelton17@gmail.com>
2024-12-17 10:06:03 -0300

    primeiro commit

---
+++ src/main/java/com/cartaoautorizador/CartaoAutorizadorApplication.java
@@ -0,0 +1,13 @@
+package com.cartaoautorizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaoAutorizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaoAutorizadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartaoautorizador/CartaoAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartaoautorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartaoAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
